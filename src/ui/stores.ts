921b9d1321ccb9e344fc50705b46ee192f269632
Matt Maiorana <matthewmaiorana@gmail.com>
2026-05-13 12:20:39 -1000

    Refactor periodic note stores

--- src/ui/stores.ts
+++ src/ui/stores.ts
@@ -2,142 +2,36 @@ import type { TFile } from "obsidian";
 import { getAllPeriodicNotes as helperGetAllPeriodicNotes } from "src/io/periodicNoteHelpers";
 import { get, writable } from "svelte/store";
 
-import { defaultSettings, ISettings } from "src/settings";
+import { defaultSettings, ISettings, Periodicity } from "src/settings";
 
 import { getDateUIDFromFile } from "./utils";
 
-function createDailyNotesStore() {
+function createPeriodicNotesStore(periodicity: Periodicity) {
   let hasError = false;
   const store = writable<Record<string, TFile>>(null);
   return {
     reindex: () => {
       const currentSettings = get(settings);
-      if (!currentSettings.daily.enabled) {
+      if (!currentSettings[periodicity].enabled) {
         store.set({});
         hasError = false;
         return;
       }
       try {
-        const notes = helperGetAllPeriodicNotes("daily", currentSettings.daily);
+        const notes = helperGetAllPeriodicNotes(
+          periodicity,
+          currentSettings[periodicity]
+        );
         store.set(notes);
         hasError = false;
       } catch (err) {
         store.set({});
         if (!hasError) {
           // Avoid error being shown multiple times
-          console.log("[Calendar] Failed to find daily notes folder", err);
-        }
-        hasError = true;
-      }
-    },
-    ...store,
-  };
-}
-
-function createWeeklyNotesStore() {
-  let hasError = false;
-  const store = writable<Record<string, TFile>>(null);
-  return {
-    reindex: () => {
-      const currentSettings = get(settings);
-      if (!currentSettings.weekly.enabled) {
-        store.set({});
-        hasError = false;
-        return;
-      }
-      try {
-        const notes = helperGetAllPeriodicNotes("weekly", currentSettings.weekly);
-        store.set(notes);
-        hasError = false;
-      } catch (err) {
-        store.set({});
-        if (!hasError) {
-          // Avoid error being shown multiple times
-          console.log("[Calendar] Failed to find weekly notes folder", err);
-        }
-        hasError = true;
-      }
-    },
-    ...store,
-  };
-}
-
-function createMonthlyNotesStore() {
-  let hasError = false;
-  const store = writable<Record<string, TFile>>(null);
-  return {
-    reindex: () => {
-      const currentSettings = get(settings);
-      if (!currentSettings.monthly.enabled) {
-        store.set({});
-        hasError = false;
-        return;
-      }
-      try {
-        const notes = helperGetAllPeriodicNotes("monthly", currentSettings.monthly);
-        store.set(notes);
-        hasError = false;
-      } catch (err) {
-        store.set({});
-        if (!hasError) {
-          // Avoid error being shown multiple times
-          console.log("[Calendar] Failed to find monthly notes folder", err);
-        }
-        hasError = true;
-      }
-    },
-    ...store,
-  };
-}
-
-function createYearlyNotesStore() {
-  let hasError = false;
-  const store = writable<Record<string, TFile>>(null);
-  return {
-    reindex: () => {
-      const currentSettings = get(settings);
-      if (!currentSettings.yearly.enabled) {
-        store.set({});
-        hasError = false;
-        return;
-      }
-      try {
-        const notes = helperGetAllPeriodicNotes("yearly", currentSettings.yearly);
-        store.set(notes);
-        hasError = false;
-      } catch (err) {
-        store.set({});
-        if (!hasError) {
-          // Avoid error being shown multiple times
-          console.log("[Calendar] Failed to find yearly notes folder", err);
-        }
-        hasError = true;
-      }
-    },
-    ...store,
-  };
-}
-
-function createQuarterlyNotesStore() {
-  let hasError = false;
-  const store = writable<Record<string, TFile>>(null);
-  return {
-    reindex: () => {
-      const currentSettings = get(settings);
-      if (!currentSettings.quarterly.enabled) {
-        store.set({});
-        hasError = false;
-        return;
-      }
-      try {
-        const notes = helperGetAllPeriodicNotes("quarterly", currentSettings.quarterly);
-        store.set(notes);
-        hasError = false;
-      } catch (err) {
-        store.set({});
-        if (!hasError) {
-          // Avoid error being shown multiple times
-          console.log("[Calendar] Failed to find quarterly notes folder", err);
+          console.log(
+            `[Calendar] Failed to find ${periodicity} notes folder`,
+            err
+          );
         }
         hasError = true;
       }
@@ -148,11 +42,11 @@ function createQuarterlyNotesStore() {
 
 // Exporting the stores
 export const settings = writable<ISettings>(defaultSettings);
-export const dailyNotes = createDailyNotesStore();
-export const weeklyNotes = createWeeklyNotesStore();
-export const monthlyNotes = createMonthlyNotesStore();
-export const yearlyNotes = createYearlyNotesStore();
-export const quarterlyNotes = createQuarterlyNotesStore(); // Added quarterlyNotes store
+export const dailyNotes = createPeriodicNotesStore("daily");
+export const weeklyNotes = createPeriodicNotesStore("weekly");
+export const monthlyNotes = createPeriodicNotesStore("monthly");
+export const yearlyNotes = createPeriodicNotesStore("yearly");
+export const quarterlyNotes = createPeriodicNotesStore("quarterly");
 
 function createSelectedFileStore() {
   const store = writable<string>(null);
